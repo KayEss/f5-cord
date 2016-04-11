190f8ed02be189758de825bdd2c8273801912856
Kirit Sælensminde <k@kirit.com>
2016-04-11 16:49:05 +0700

    Add some tests for `lstring`.

--- test/run/lstring-std_string.cpp
+++ test/run/lstring-std_string.cpp
@@ -1,10 +1,18 @@
 #include <cassert>
 #include <f5/cord/lstring.hpp>
 
+constexpr f5::lstring empty;
 constexpr f5::lstring html = "html";
 
 
 int main() {
-    return std::string(html) == "html" ? 0 : 1;
+    assert(std::string(empty).empty());
+
+    assert(std::string(html) == "html");
+
+    std::string shtml = html;
+    assert(std::string(shtml) == "html");
+
+    return 0;
 }
 
