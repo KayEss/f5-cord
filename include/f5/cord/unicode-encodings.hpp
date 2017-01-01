cafba051736f8321ba623193aad8c67032858c3e
Kirit Sælensminde <k@kirit.com>
2017-01-01 12:01:30 +0700

    Add in iterators for converting between some combinations of UTF encodings

--- include/f5/cord/unicode-encodings.hpp
+++ include/f5/cord/unicode-encodings.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2016, Felspar Co Ltd. http://www.kirit.com/f5
+    Copyright 2016-2017, Felspar Co Ltd. http://www.kirit.com/f5
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -43,6 +43,21 @@ namespace f5 {
                 return true;
             }
             return false;
+        }
+        /// Return a valid code point (or the replacement character)
+        template<typename E = std::domain_error>
+        constexpr inline
+        utf32 return_valid(utf32 cp) {
+            if ( cp >= 0xd800 && cp <= 0xdbff ) {
+                raise<E>("UTF32 code point is in the leading UTF16 surrogate pair range");
+            } else if ( cp >= 0xdc00 && cp <= 0xdfff ) {
+                raise<E>("UTF32 code point is in the trailing UTF16 surrogate pair range");
+            } else if ( cp > 0x10ffff ) {
+                raise<E>("UTF32 code point is beyond the allowable range");
+            } else {
+                return cp;
+            }
+            return 0xfffd;
         }
 
         /// Return the number of UTF8 values that this code point will
@@ -92,6 +107,50 @@ namespace f5 {
                 raise<E>("Cannot encode an invalid UTF32 code point ");
                 return {0u, {{0u, 0u, 0u, 0u}}};
             }
+        }
+
+
+        /// Return the number of UTF16 code points needed to encode
+        /// the specified UTF32 code point.
+        template<typename E = std::domain_error>
+        constexpr inline
+        char u16length(utf32 cp) {
+            if ( not check_valid(cp) ) return 0;
+            else if ( cp < 0x1'0000 ) return 1;
+            else return 2;
+        }
+
+
+        /// Return either a single UTF16 code point or a surrogate pair
+        /// that describes the UTF32 code point passed in
+        template<typename E = std::domain_error>
+        constexpr inline
+        std::pair<char, std::array<utf16, 2>> u16encode(utf32 cp) {
+            auto length = u16length(cp);
+            if ( length == 1 ) {
+                return {1, {{static_cast<utf16>(cp), 0u}}};
+            } else if ( length == 0 ) {
+                raise<E>("Cannot convert an invalid code point to UTF-16");
+                return {0, {{0, 0}}};
+            } else {
+                return {2, {{
+                    static_cast<utf16>(0xD800 - (0x10000 >> 10) + (cp >> 10)),
+                    static_cast<utf16>(0xDC00 + (cp & 0x3FF))}}};
+            }
+        }
+
+
+        /// Return true if this UTF-16 code represents either part of a
+        /// surrogate pair
+        constexpr inline bool is_surrogate(utf16 cp) {
+            return cp >= 0xd800 && cp <= 0xdfff;
+        }
+
+
+        /// Decode a surrogate pair
+        template<typename E = std::domain_error>
+        constexpr inline utf32 u16decode(utf16 first, utf16 second) {
+            return return_valid<E>((utf32(first) << 10) + second + 0x10000 - ( 0xD800 << 10 ) - 0xDC00);
         }
 
 
--- test/compile/unicode.cpp
+++ test/compile/unicode.cpp
@@ -1,3 +1,11 @@
+/*
+    Copyright 2016-2017, Felspar Co Ltd. http://www.kirit.com/f5
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
 #include <f5/cord/unicode.hpp>
 
 
@@ -16,3 +24,13 @@ static_assert(f5::u8length(0x1d11e) == 4, "The treble clef is 4 bytes long");
 
 // u8encode
 constexpr auto space = f5::u8encode(' ');
+
+
+// UTF-16
+static_assert(not f5::is_surrogate(' '), "Space");
+static_assert(f5::is_surrogate(0xd834), "High char for treble clef");
+static_assert(f5::is_surrogate(0xdd1e), "Low char for treble clef");
+static_assert(not f5::is_surrogate(0xfffd), "Replacement char");
+
+static_assert(f5::u16decode(0xd834, 0xdd1e) == 0x1d11e, "Treble clef");
+static_assert(f5::u16decode<void>(0xdd1e, 0xd834) == 0xfffd, "Substituted");
