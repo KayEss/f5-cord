7ee1a37ac794284fee9a3ffdaa97a2acc72fe73e
Kirit Sælensminde <k@kirit.com>
2018-10-09 11:08:00 +0700

    Start to lay out the shared string implementation.

---
+++ include/f5/cord/unicode-shared_string.hpp
@@ -0,0 +1,32 @@
+/**
+    Copyright 2018, Felspar Co Ltd. <https://kirit.com/f5>
+
+    Distributed under the Boost Software License, Version 1.0.
+    See <http://www.boost.org/LICENSE_1_0.txt>
+*/
+
+
+#pragma once
+
+
+#include <f5/cord/unicode-core.hpp>
+
+
+namespace f5 {
+
+
+    inline namespace cord {
+
+
+        /// UTF8 string with shared ownership.
+        class shared_string {
+            u8shared buffer;
+        public:
+        };
+
+
+    }
+
+
+}
+
